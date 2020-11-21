83a9b227143dc28696c67287f19d1af1dff1d7c0
Tommaso Siligardi <tomesse@outlook.it>
2020-11-21 18:19:20 +0100

    aggiunto hbs

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express= require ('express');
 const app= express();
 
+const handlebars = require('express-handlebars')
+app.set('view engine', 'hbs')
+app.use(express.static('public'))
+
+app.engine('hbs', handlebars({
+    layoutsDir: __dirname + '/views/layouts',
+    extname: 'hbs',
+    defaultLayout: 'index', //indica il layout di default
+    }));
 
 app.get('/',(req,res)=>{
-    res.send(`ciao ${req.query.nome}`)
+    res.render('main',{name: req.query.nome})
 })
 
 app.get('/mare',(req,res)=>{
